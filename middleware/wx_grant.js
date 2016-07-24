6a4aff768e8a7a977f38e3dcce8892c226605b40
yeahsing <yeahsing13@hotmail.com>
2016-07-25 06:29:21 +0800

    validate

--- middleware/wx_grant.js
+++ middleware/wx_grant.js
@@ -1,6 +1,7 @@
 var AV = require('leanengine')
 module.exports = function(req, res, next){
     "use strict";
+    console.log(req.sessionToken)
     if(req.currentUser){
         next();
     }else{
